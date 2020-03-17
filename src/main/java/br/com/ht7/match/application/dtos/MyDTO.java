c54b47cbd842c0ef2344d3e3813c8ad45d8fc5a4
Tiago Guizelini <tguizelini@macbook-pro.local>
2020-03-17 13:24:27 -0300

    refact

---
+++ src/main/java/br/com/ht7/match/application/dtos/MyDTO.java
@@ -0,0 +1,14 @@
+package br.com.ht7.match.application.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyDTO {
+    private String sendWhatYouWhant;
+}
