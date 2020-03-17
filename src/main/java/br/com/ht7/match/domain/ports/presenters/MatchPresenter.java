f2aeb6dc3fd6066372e9fdd78bcaf7e1b0c0f821
Tiago Guizelini <tguizelini@macbook-pro.local>
2020-03-17 13:17:07 -0300

    refact

--- src/main/java/br/com/ht7/match/domain/ports/presenters/MatchPresenter.java
+++ src/main/java/br/com/ht7/match/domain/ports/presenters/MatchPresenter.java
@@ -1,9 +1,6 @@
 package br.com.ht7.match.domain.ports.presenters;
 
-import br.com.ht7.match.domain.Match;
 import br.com.ht7.match.domain.usecases.MatchUseCase;
-
-import java.util.Optional;
 
 public interface MatchPresenter extends MatchUseCase {
 
